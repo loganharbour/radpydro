028d795be23e0717b12f4a27f388da4dea08407a
Zachary K. Hardy <zach.hardy@ne-morelgrad01.ne.tamu.edu>
2019-03-28 18:29:35 -0500

    Revamped radiation predictor code.
    Made various corrections added comments and restructured.

--- lagrangian_radiation_predictor.py
+++ lagrangian_radiation_predictor.py
@@ -6,49 +6,39 @@ from scipy.linalg import solve_banded
 class LagrangianRadiationPredictor:
 
     def __init__(self, rp):
-
         self.rp = rp
         self.input = rp.input
         self.geo = rp.geo
         self.mat = rp.mat
         self.fields = rp.fields
 
+        # init containers for linear system
         self.diag = np.zeros(self.geo.N)
         self.lowerdiag = np.zeros(self.geo.N)
         self.upperdiag = np.zeros(self.geo.N)
         self.rhs = np.zeros(self.geo.N)
 
+        # init containers for k'th time-step variables
         self.rho_pk = np.zeros(self.geo.N)
         self.dr_pk = np.zeros(self.geo.N)
         self.u_pk = np.zeros(self.geo.N + 1)
         self.A_pk = np.zeros(self.geo.N + 1)
 
+        # additional parameters
         self.nu = np.zeros(self.geo.N)
         self.xi = np.zeros(self.geo.N)
 
-    def assembleSystem(self, dt):
-
-        self.computeAuxiliaryFields(dt)
-
-        self.assembleInnerCells(dt)
-
-        self.applyLeftBoundary(dt)
-
-        self.applyRightBoundary(dt)
-
     def computeAuxiliaryFields(self, dt):
-
-        m = self.mat.m
         a = self.input.a
         c = self.input.c
         C_v = self.mat.C_v
 
+        m = self.mat.m
         rho_old = self.fields.rho_old
         dr_old = self.geo.dr_old
         u_old = self.fields.u_old
         A_old = self.geo.A_old
         P_old = self.fields.P_old
-
         T_old = self.fields.T_old
 
         rho_p = self.fields.rho_p
@@ -56,75 +46,34 @@ class LagrangianRadiationPredictor:
         u_p = self.fields.u_p
         A_p = self.geo.A_p
 
+        # compute k'th time-step variables
         self.rho_pk = (rho_p + rho_old) / 2
         self.dr_pk = (dr_p + dr_old) / 2
         self.u_pk = (u_p + u_old) / 2
         self.A_pk = (A_p + A_old) / 2
 
+        # recompute opacities at old temperatures
         self.mat.recomputeKappa_t(T_old)
         self.mat.recomputeKappa_a(T_old)
 
         kappa_t = self.mat.kappa_t
         kappa_a = self.mat.kappa_a
 
+        # parameter in radiation energy system (Eq. 20a)
         self.nu = dt * kappa_a * c * 2 * a * T_old**3
         self.nu /= C_v + dt * kappa_a * c * 2 *a * T_old**3
 
+        # parameter in radiation energy system (Eq. 20b)
         for i in range(0, self.geo.N):
             self.xi[i] = - P_old[i] * (A_old[i+1] * self.u_pk[i+1] - A_old[i] * self.u_pk[i])
 
-    def assembleInnerCells(self, dt):
-
-        m = self.mat.m
-        a = self.input.a
-        c = self.input.c
-        C_v = self.mat.C_v
-
-        rho_old = self.fields.rho_old
-        A_old = self.geo.A_old
-        E_old = self.fields.E_old
-        T_old = self.fields.T_old
-
-        rho_pk = self.rho_pk
-        dr_pk = self.dr_pk
-        u_pk = self.u_pk
-        A_pk = self.A_pk
-
-        rho_p = self.fields.rho_p
-
-        kappa_t = self.mat.kappa_t
-        kappa_a = self.mat.kappa_a
-
-        nu = self.nu
-        xi = self.xi
-
+    def assembleSystem(self, dt):
         N = self.geo.N
-
-        for i in range(1, N-1):
-
-            denom1 = 3 * (rho_pk[i] * dr_pk[i] * kappa_t[i+1] + rho_pk[i+1] * dr_pk[i+1] * kappa_t[i+1])
-            denom2 = 3 * (rho_pk[i-1] * dr_pk[i-1] * kappa_t[i] + rho_pk[i] * dr_pk[i] * kappa_t[i])
-
-            self.diag[i] += m[i] / (dt * rho_p[i]) + A_pk[i+1] * c / denom1 + A_pk[i] * c / denom2
-            self.diag[i] += m[i] / 2 * (1 - nu[i]) * m[i] * c * kappa_a[i]
-
-            self.upperdiag[i+1] = - A_pk[i+1] * c / denom1
-            self.lowerdiag[i-1] = - A_pk[i] * c / denom2
-
-            self.rhs[i] += (- m[i] / (dt * rho_old[i])  \
-                       - m[i] / 2 * kappa_a[i] * c * (1 - nu[i]) \
-                       - 1 / 3 * (A_old[i+1] * u_pk[i+1] - A_old[i] * u_pk[i]))*E_old[i]
-            self.rhs[i] +=  nu[i] * xi[i]
-            self.rhs[i] +=  A_pk[i+1] * c / denom1 * (E_old[i+1] - E_old[i])
-            self.rhs[i] +=  A_pk[i] * c / denom2 * (E_old[i] - E_old[i-1])
-
-    def applyLeftBoundary(self, dt):
-
-        m = self.mat.m
         a = self.input.a
         c = self.input.c
         C_v = self.mat.C_v
 
+        m = self.mat.m
         rho_old = self.fields.rho_old
         A_old = self.geo.A_old
         E_old = self.fields.E_old
@@ -143,116 +92,97 @@ class LagrangianRadiationPredictor:
         nu = self.nu
         xi = self.xi
 
-        denom1 = 3 * (rho_pk[0] * dr_pk[0] * kappa_t[1] + rho_pk[1] * dr_pk[1] * kappa_t[1])
+        for i in range(0, N):
+            # time derivative contributions
+            self.diag[i] += m[i] / (dt * rho_p[i])
+            self.rhs[i] += m[i] / (dt * rho_p[i]) * E_old[i]
 
-        if self.input.rad_L is 'reflective':
+            # absorption contributions
+            self.diag[i] += m[i] * kappa_a[i] * c * (1 - nu[i]) / 2
+            self.rhs[i] -= m[i] * kappa_a[i] * c * (1 - nu[i]) * E_old[i] / 2
 
-            self.diag[0] += m[0] / (dt * rho_p[0]) + A_pk[1] * c / denom1
-            self.diag[0] += m[0] / 2 * (1 - nu[0]) * c * kappa_a[0]
+            # material emmision and nu * xi contributions
+            self.rhs[i] += m[i] * kappa_a[i] * c * (1 - nu[i]) * a * T_old[i]**4
+            self.rhs[i] += nu[i] * xi[i]
 
-            self.upperdiag[0] = - A_pk[1] * c / denom1
+            # drift term contributions
+            self.rhs[i] -= 1/3 * E_old[i] * (A_pk[i+1] * u_pk[i+1] \
+                                             - A_pk[i] * u_pk[i])
 
-            self.rhs[0] += (- m[0] / (dt * rho_old[0])  \
-                           - m[0] / 2 * kappa_a[0] * c * (1 - nu[0]) \
-                           - 1 / 3 * (A_old[1] * u_pk[1] - A_old[0] * u_pk[0]))*E_old[0]
-            self.rhs[0] += nu[0] * xi[0]
-            self.rhs[0] += A_pk[1] * c / denom1 * (E_old[1] - E_old[0])
+            # if past leftmost cell
+            if (i > 0):
+                # left edge flux coefficient
+                coeff_F_L = 2 * c / (3 * (rho_pk[i-1] * dr_pk[i-1] * kappa_t[i] \
+                                          + rho_pk[i] * dr_pk[i] * kappa_t[i]))
 
-        else:
+                # left edge flux contributions
+                self.diag[i] += A_pk[i] * coeff_F_L / 2
+                self.lowerdiag[i-1] -= A_pk[i] * coeff_F_L / 2
+                self.rhs[i] -= A_pk[i] * coeff_F_L * E_old[i] / 2
+                self.rhs[i-1] += A_pk[i] * coeff_F_L * E_old[i-1] / 2
 
-            E_left = self.input.rad_L_val
-            T_left = ((1 / a * E_left + T_old[0]**4) / 2)**(1 / 4)
-            kappa_left = self.mat.kappa_func(T_left) + self.mat.kappa_s
+            # if before rightmost cell
+            if (i < N-1):
+                # right edge flux coefficient
+                coeff_F_R = 2 * c / (3 * (rho_pk[i] * dr_pk[i] * kappa_t[i+1] \
+                                          + rho_pk[i+1] * dr_pk[i+1] * kappa_t[i+1]))
 
-            denom2 = 3 * rho_pk[0] * dr_pk [0] * kappa_left + 4
+                # right edge flux contributions
+                self.diag[i] += A_pk[i+1] * coeff_F_R / 2
+                self.upperdiag[i+1] -= A_pk[i+1] * coeff_F_R / 2
+                self.rhs[i] -= A_pk[i+1] * coeff_F_R * E_old[i] / 2
+                self.rhs[i+1] += A_pk[i+1] * coeff_F_R * E_old[i+1] / 2
 
-            self.diag[0] += m[0] / (dt * rho_p[0]) + A_pk[1] * c / denom1
-            self.diag[0] += A_pk[0] * c / denom2
-            self.diag[0] += m[0] / 2 * (1 - nu[0]) * c * kappa_a[0]
+            # if on left boundary cell
+            if (i == 0):
+                if self.input.rad_L is 'source':
+                    E_L = self.input.rad_L_val
 
-            self.upperdiag[0] = - A_pk[1] * c / denom1
+                    # compute left bdry temp with Eq. 34
+                    T_L = ((E_L / a + T_old[i]**4) / 2)**(1/4)
+                    # compute left bdry total opacity with T_L
+                    kappa_t_L = self.mat.kappa_func(T_L) + self.mat.kappa_s
 
-            self.rhs[0] += (- m[0] / (dt * rho_old[0])  \
-                            - m[0] / 2 * kappa_a[0] * c * (1 - nu[0]) \
-                            - 1 / 3 * (A_old[1] * u_pk[1] - A_old[0] * u_pk[0]))*E_old[0]
-            self.rhs[0] += nu[0] * xi[0]
-            self.rhs[0] += c / denom1 * (E_old[1] - E_old[0])
-            self.rhs[0] += - A_pk[0] * 2 * c / denom2 * E_old[0]
-            self.rhs[0] += A_pk[0] * 2 * c / denom2 * E_left
+                    # left bdry flux coefficient
+                    coeff_F_Lb = 2 * c / (3 * rho_pk[i] * dr_pk[i] * kappa_t_L + 4)
 
-    def applyRightBoundary(self, dt):
+                    # left bdry flux contributions
+                    self.diag[i] += A_pk[i] * coeff_F_Lb / 2
+                    self.rhs[i] += A_pk[i] * coeff_F_Lb * E_L / 2
 
-        m = self.mat.m
-        a = self.input.a
-        c = self.input.c
-        C_v = self.mat.C_v
+                elif self.input.rad_L is 'reflective':
+                    pass
 
-        rho_old = self.fields.rho_old
-        A_old = self.geo.A_old
-        E_old = self.fields.E_old
-        T_old = self.fields.T_old
-        p_old = self.fields.P_old
+            # if on right boundary cell
+            if (i == N-1):
+                    if self.input.rad_R is 'source':
+                        E_R = self.input.rad_R_val
 
-        rho_pk = self.rho_pk
-        dr_pk = self.dr_pk
-        u_pk = self.u_pk
-        A_pk = self.A_pk
+                        # compute right bdry temp with Eq. 36
+                        T_R = ((E_R / a + T_old[i]**4) / 2)**(1/4)
+                        # compute right bdry total opacity with T_R
+                        kappa_t_R = self.mat.kappa_func(T_R) + self.mat.kappa_s
 
-        rho_p = self.fields.rho_p
+                        # right bdry flux coefficient
+                        coeff_F_Rb = 2 * c / (3 * rho_pk[i] * dr_pk[i] * kappa_t_R + 4)
 
-        kappa_t = self.mat.kappa_t
-        kappa_a = self.mat.kappa_a
+                        #right bdry flux contributions
+                        self.diag[i] += A_pk[i+1] * coeff_F_Rb / 2
+                        self.rhs[i] += A_pk[i+1] * coeff_F_Rb * E_R / 2
+                        print('\n', A_pk[i+1] * coeff_F_Rb * E_R / 2)
 
-        nu = self.nu
-        xi = self.xi
-
-        N = self.geo.N
-
-        denom2 = 3 * (rho_pk[N-2] * dr_pk[N-2] * kappa_t[N-1] + rho_pk[N-1] * dr_pk[N-1] * kappa_t[N-1])
-
-        if self.input.rad_R is 'reflective':
-
-            self.diag[N-1] += m[N-1] / (dt * rho_p[N-1]) + A_pk[N-1] * c / denom2
-            self.diag[N-1] += m[N-1] / 2 * (1 - nu[N-1]) * m[N-1] * c * kappa_a[N-1]
-
-            self.lowerdiag[N-2] = - A_pk[N-1] * c / denom2
-
-            self.rhs[N-1] += (- m[N-1] / (dt * rho_old[N-1])  \
-                        - m[N-1] / 2 * kappa_a[N-1] * c * (1 - nu[N-1]) \
-                        - 1 / 3 * (A_old[N] * u_pk[N] - A_old[N-1] * u_pk[N-1]))*E_old[N-1]
-            self.rhs[N-1] += nu[N-1] * xi[N-1]
-            self.rhs[N-1] += - A_pk[N-1] * c / denom2 * (E_old[N-1] - E_old[N-2])
-
-        else:
-
-            E_right = self.input.rad_R_val
-            T_right = ((1 / a * E_right + T_old[N-1]**4) / 2)**(1 / 4)
-            kappa_right = self.mat.kappa_func(T_right) + self.mat.kappa_s
-
-            denom1 = 3 * rho_pk[N-1] * dr_pk[N-1] * kappa_right + 4
-
-            self.diag[N-1] += m[N-1] / (dt * rho_p[N-1]) + A_pk[N] * c / denom1 + A_pk[N-1] * c / denom2
-            self.diag[N-1] += m[N-1] / 2 * (1 - nu[N-1]) * m[N-1] * c * kappa_a[N-1]
-
-            self.lowerdiag[N-2] = - A_pk[N-1] * c / denom2
-
-            self.rhs[N-1] += (- m[N-1] / (dt * rho_old[N-1])  \
-                       - m[N-1] / 2 * kappa_a[N-1] * c * (1 - nu[N-1]) \
-                       - 1 / 3 * (A_old[N] * u_pk[N] - A_old[N-1] * u_pk[N-1]))*E_old[N-1]
-            self.rhs[N-1] += nu[N-1] * xi[N-1]
-            self.rhs[N-1] += - A_pk[N-1] * c / denom2 * (E_old[N-1] - E_old[N-2])
-            self.rhs[N-1] += - A_pk[N] * c / denom1 * (E_old[N-1])
-            self.rhs[N-1] += A_pk[N] * 2 * c * E_right / denom1
+                    elif self.input.rad_R is 'reflective':
+                        pass
 
     def solveSystem(self, dt):
 
+        self.computeAuxiliaryFields(dt)
         self.assembleSystem(dt)
 
         data = np.array([self.lowerdiag, self.diag, self.upperdiag])
         diags = np.array([-1, 0, 1])
 
         systemMatrix = spdiags(data, diags, self.geo.N, self.geo.N, format = 'csr')
-
         self.fields.E_p = spsolve(systemMatrix, self.rhs)
 
         self.recomputeInternalEnergy(dt)
@@ -271,9 +201,9 @@ class LagrangianRadiationPredictor:
 
         xi = self.xi
 
-        E_k = 0.5 * (self.fields.E_p + self.fields.E_old)
+        E_k = (self.fields.E_p + self.fields.E_old) / 2
 
         increment = dt * C_v * (m * kappa_a * c * (E_k - a * T_old**4) + xi)
-        increment /= m*C_v + dt * m * kappa_a * c * 2 * a * T_old**3
+        increment /= m * C_v + dt * m * kappa_a * c * 2 * a * T_old**3
 
         self.fields.e_p = e_old + increment
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from radpydro import RadPydro
 import numpy as np
 
 input = InputParameters()
-input.geometry = 'cylindrical'
+input.geometry = 'slab'
 input.r_half = np.linspace(0, 5, num=6)
 input.C_v = 1.0
 input.gamma = 1.5
@@ -11,18 +11,20 @@ input.kappa = [1, 1, 1, 1]
 input.kappa_s = 1 #
 input.a = 0.01372 # [jerks / (cm3 kev4)]
 input.c = 299.792 # [cm / sh]
-input.E = lambda r: 1.0
+input.E = lambda r: 0.
 input.rho = lambda r: 1.0
 input.T = lambda r: 273.15
-input.u = lambda r: 1.0
+input.u = lambda r: 0.
 
 # Boundary conditions
-input.hydro_L = 'P'
-input.hydro_L_val = 5
-input.hydro_R = 'P'
-input.hydro_R_val = 5
+input.hydro_L = 'u'
+input.hydro_L_val = None
+input.hydro_R = 'u'
+input.hydro_R_val = None
 input.rad_L = 'reflective'
+input.rad_L_val = None
 input.rad_R = 'reflective'
+input.rad_R_val = None
 
 # Iteration controls
 input.CoFactor = 1
@@ -35,11 +37,25 @@ rp.computeTimeStep()
 rp.hydro.solveVelocity(rp.timeSteps[-1], True)
 rp.geo.moveMesh(rp.timeSteps[-1], True)
 rp.fields.recomputeRho(True)
-print(rp.fields.u_old, rp.fields.u_p)
-print(rp.fields.rho_old, rp.fields.rho_p)
+rp.radPredictor.solveSystem(rp.timeSteps[-1])
+
+print('\nInitial Cell Edges: ', rp.geo.r_half_old)
+print('Predictor Step Cell Edges: ', rp.geo.r_half_p)
+
+print('\nInitial Density: ', rp.fields.rho_old)
+print('Predictor Step Density: ', rp.fields.rho_p)
+
+print('\nInitial Velocity: ', rp.fields.u_old)
+print('Predictor Step Velocity: ', rp.fields.u_p)
+
+print('\nInitial Internal Energy', rp.fields.e_old)
+print('Predictor Step Internal Energy: ', rp.fields.e_p)
+
+print('\nInitial Radiation Energy: ', rp.fields.E_old)
+print('Predictor Step Radiation Energy: ', rp.fields.E_p)
+
+print('\nInitial Pressure: ', rp.fields.P_old)
+print('\nInitial Temperature: ', rp.fields.T_old)
 
 
-
-#rp.radPredictor.solveSystem(rp.timeSteps[-1])
 #rp.radCorrector.solveSystem(rp.timeSteps[-1])
-
