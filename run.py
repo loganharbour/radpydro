593d0e3285310d22633b8d7ea42d8b2e85b3c799
Zachary K. Hardy <zach.hardy@ne-morelgrad01.ne.tamu.edu>
2019-04-10 14:06:31 -0500

    Hydro only calculation made simpler.
    Only one parameter, enable_radiation is used.
    Added extra layer of if statements to recomputeInternalEnergy for hydro only.
    If hydro only, inputparameters sets E IC and BCs to 0 automatically.

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 input = InputParameters()
+input.enable_radiation = False
 input.geometry = 'slab'
-input.N = 100
+input.N = 250
 input.R = 1
 input.Tf = 0.5
 input.r_half = np.linspace(0, input.R, num=input.N + 1) # cm
@@ -19,12 +20,12 @@ input.c = 299.792 # [cm / sh]
 # Initial conditions
 input.rho = lambda r: 1.    #g/cm3
 input.T = lambda r: 1.     # keV
-input.u = lambda r: 0.    # cm/sh
+input.u = lambda r: 0.   # cm/sh
 input.E = lambda r: input.a * input.T(0)**4
 
 # Boundary conditions
 input.hydro_L = 'P'
-input.hydro_L_val = 0
+input.hydro_L_val = (input.gamma - 1) * input.C_v * input.T(0) * input.rho(0)
 input.hydro_R = 'P'
 input.hydro_R_val = 0
 input.rad_L = 'source'
